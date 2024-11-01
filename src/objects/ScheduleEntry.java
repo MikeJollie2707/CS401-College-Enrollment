c85e435cc0f219cba43ed312b65277b1756b1ac5
MikeJollie2707 <bachngo@yahoo.com>
2024-11-01 16:37:54 -0700

    Fix some types

--- src/objects/ScheduleEntry.java
+++ src/objects/ScheduleEntry.java
@@ -1,30 +1,39 @@
 package objects;
 
-import java.sql.Time;
+import java.time.DayOfWeek;
+import java.time.OffsetTime;
 
 public class ScheduleEntry {
     private String location;
     private boolean is_sync;
-    private String day_of_week;
-    private Time start_time;
-    private Time end_time;
+    private DayOfWeek day_of_week;
+    private OffsetTime start_time;
+    private OffsetTime end_time;
 
-    public ScheduleEntry(String location, boolean is_sync, String day_of_week, Time start_time, Time end_time) {
+    public ScheduleEntry(String location, boolean is_sync, DayOfWeek day_of_week, OffsetTime start_time,
+            OffsetTime end_time) {
         this.location = location;
         this.is_sync = is_sync;
         this.day_of_week = day_of_week;
         this.start_time = start_time;
         this.end_time = end_time;
     }
-    public Tuple getTime() {
-        return new Tuple(start_time, end_time);
+
+    public OffsetTime[] getTime() {
+        OffsetTime[] temp = new OffsetTime[2];
+        temp[0] = start_time;
+        temp[1] = end_time;
+        return temp;
     }
+
     public String getLocation() {
         return location;
     }
-    public String getDayOfWeek() {
+
+    public DayOfWeek getDayOfWeek() {
         return day_of_week;
     }
+
     public boolean isSync() {
         return is_sync;
     }
