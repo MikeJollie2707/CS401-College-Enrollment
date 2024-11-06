94de59f516e4b2b36003dcd43ba125d8e6014fe9
MikeJollie2707 <bachngo@yahoo.com>
2024-11-06 12:21:37 -0800

    Implement requirement 3.1.2.5

--- src/objects/Section.java
+++ src/objects/Section.java
@@ -100,11 +100,58 @@ public class Section implements Serializable {
         this.active = state;
     }
 
+    /**
+     * Set the max capacity of this course to the specified value.
+     * <p>
+     * If the provided value is smaller than the current enroll list, the method
+     * will drop as many students as possible (starting from last place) to reach
+     * the new value.
+     * <p>
+     * If the provided value is larger than the current enroll list, the method will
+     * move as many students as possible from the waitlist (starting from the first
+     * place) to reach the new value.
+     * 
+     * @param cap The new course capacity.
+     */
     public void setMaxCapacity(int cap) {
+        if (cap <= 0) {
+            // TODO: Do something about this.
+
+        }
+
+        if (cap < enrolled.size()) {
+            while (cap < enrolled.size()) {
+                Student student = enrolled.removeLast();
+                waitlisted.addFirst(student);
+            }
+            setMaxWaitSize(max_wait);
+        } else if (cap > enrolled.size()) {
+            while (cap > enrolled.size()) {
+                moveToEnroll();
+            }
+        }
+
         this.max_capacity = cap;
     }
 
+    /**
+     * Set the max waitlist size to the specified value and drop students that
+     * exceed this value.
+     * <p>
+     * If wait < waitlisted.size(), this method will drop students from the last
+     * student to the most current.
+     * 
+     * @param wait A positive value indicating the max size of the waitlist.
+     */
     public void setMaxWaitSize(int wait) {
+        if (wait <= 0) {
+            // TODO: Do something about this.
+        }
+
+        while (wait < waitlisted.size()) {
+            waitlisted.removeLast();
+        }
+
         this.max_wait = wait;
     }
 
@@ -114,5 +161,24 @@ public class Section implements Serializable {
 
     public void setSchedule(ScheduleEntry[] schedule) {
         this.schedule = schedule;
+    }
+
+    /**
+     * Attempt to shift a student from the first place in waitlist to the enrollment
+     * list.
+     * <p>
+     * This method does nothing if there's no one in the waitlist or if the
+     * enrollment list is already full.
+     */
+    private void moveToEnroll() {
+        if (waitlisted.size() == 0) {
+            return;
+        }
+        if (enrolled.size() >= max_capacity) {
+            return;
+        }
+
+        Student student = waitlisted.removeFirst();
+        enrolled.addLast(student);
     }
 }
