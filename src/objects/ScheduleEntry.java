5f049e642437e0dbfa39eef84dce71a7fdcffcf9
MikeJollie2707 <bachngo@yahoo.com>
2024-11-06 14:56:06 -0800

    Add time check for schedule entry

--- src/objects/ScheduleEntry.java
+++ src/objects/ScheduleEntry.java
@@ -15,6 +15,11 @@ public class ScheduleEntry {
         this.location = location;
         this.is_sync = is_sync;
         this.day_of_week = day_of_week;
+
+        if (end_time.isBefore(start_time) && end_time.isEqual(start_time)) {
+            throw new IllegalArgumentException("'end_time' must be strictly after 'start_time'");
+        }
+
         this.start_time = start_time;
         this.end_time = end_time;
     }
