1e088e46075d4de1fd58693b517f6721821871f4
MikeJollie2707 <bachngo@yahoo.com>
2024-11-06 16:05:18 -0800

    WIP university init in server

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -10,12 +10,15 @@ import java.net.Socket;
 import objects.Account;
 import objects.ClientMsg;
 import objects.ServerMsg;
+import objects.University;
 
 public class ClientHandler implements Runnable {
     final private Socket socket;
+    final private University[] universities;
 
-    public ClientHandler(Socket socket) {
+    public ClientHandler(Socket socket, University[] universities) {
         this.socket = socket;
+        this.universities = universities;
     }
 
     @Override
@@ -24,6 +27,15 @@ public class ClientHandler implements Runnable {
                 OutputStream raw_ostream = socket.getOutputStream()) {
             ObjectOutputStream ostream = new ObjectOutputStream(raw_ostream);
             try {
+                // NOTE: Whether or not the client initiate the request
+                // or the server automatically send this info, need more
+                // discussion with whoever is maintaining the client code.
+                String[] uniNames = new String[universities.length];
+                for (int i = 0; i < uniNames.length; ++i) {
+                    uniNames[i] = universities[i].getName();
+                }
+                ostream.writeObject(ServerMsg.asOK(uniNames));
+
                 ObjectInputStream istream = new ObjectInputStream(raw_istream);
                 ClientMsg req = (ClientMsg) istream.readObject();
                 if (req.isEndpoint("GET", "foo")) {
--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,9 +1,16 @@
 package server;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import objects.University;
 
 public class ServerMain {
     public static void main(String[] args) {
@@ -12,18 +19,48 @@ public class ServerMain {
             InetAddress localhost = InetAddress.getLocalHost();
             System.out.println(String.format("Listening on %s:%d", localhost.getHostAddress(), 7777));
 
-            // TODO: Setup universities
+            University[] universities = loadInfoFromFile("unis.txt");
 
             while (true) {
                 Socket socket = ss.accept();
                 System.out.println("Client connected: " + socket.getInetAddress());
 
-                ClientHandler handler = new ClientHandler(socket);
+                ClientHandler handler = new ClientHandler(socket, universities);
                 new Thread(handler).start();
             }
 
         } catch (IOException err) {
             err.printStackTrace();
         }
+    }
+
+    private static University[] loadInfoFromFile(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        try (Scanner scanner = new Scanner(file)) {
+            // nextInt() doesn't move the scanner, yay...
+            int num = Integer.valueOf(scanner.nextLine());
+            HashMap<String, University> uniMap = new HashMap<>();
+            for (int i = 0; i < num; ++i) {
+                String row = scanner.nextLine();
+                String[] entry = row.split(",");
+                uniMap.put(entry[0], new University(entry[0], entry[1]));
+
+                // TODO: Additional info for universities can be provided in further columns.
+                // For now it only loads the bare minimum for the constructor.
+                // If for example, we also want to load admin+student info from file as well,
+                // one of the column can specify the filename in which it can get that kind of
+                // info.
+                // So something like "CSUEB,Carlos Bee,csueb_admins.txt,csueb_students.txt"
+            }
+
+            University[] unis = uniMap.values().stream()
+                    .collect(Collectors.toList())
+                    .toArray(new University[0]); // Weird syntax but it works, check the docs
+
+            return unis;
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+        return null;
     }
 }
