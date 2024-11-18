35cf801717d0dca9cbcdd34f3b9fdb7e206ceede
MikeJollie2707 <bachngo@yahoo.com>
2024-11-18 12:59:35 -0800

    Change `BodyEnrollOrDropAs` to contain section instead

--- src/objects/BodyEnrollOrDropAs.java
+++ src/objects/BodyEnrollOrDropAs.java
@@ -2,22 +2,22 @@ package objects;
 
 public class BodyEnrollOrDropAs {
     private String studentID;
-    private Course course;
+    private Section section;
 
-    public BodyEnrollOrDropAs(String studentID, Course course) {
-        if (studentID == null || course == null) {
+    public BodyEnrollOrDropAs(String studentID, Section section) {
+        if (studentID == null || section == null) {
             throw new NullPointerException("Arguments for constructor must not be null.");
         }
 
         this.studentID = studentID;
-        this.course = course;
+        this.section = section;
     }
 
     public String getStudentID() {
         return studentID;
     }
 
-    public Course getCourse() {
-        return course;
+    public Section getSection() {
+        return section;
     }
 }
