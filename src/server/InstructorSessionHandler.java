01875a6b8ffa8932bec2681010063f029b86f2a4
MikeJollie2707 <bachngo@yahoo.com>
2024-12-01 20:11:49 -0800

    Pseudo instructor route

--- src/server/InstructorSessionHandler.java
+++ src/server/InstructorSessionHandler.java
@@ -5,10 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-import objects.ClientMsg;
-import objects.Instructor;
-import objects.ServerMsg;
-import objects.University;
+import objects.*;
 
 public class InstructorSessionHandler extends SessionHandler {
     private Instructor instructor;
@@ -43,12 +40,19 @@ public class InstructorSessionHandler extends SessionHandler {
                     }
                 }
 
-                if (req.isEndpoint("GET", "sections")) {
-                    resp = viewSections(req);
+                try {
+                    if (req.isEndpoint("GET", "sections")) {
+                        resp = viewSections(req);
+                    }
+                }
+                catch (Exception err) {
+                    err.printStackTrace();
+                    resp = ServerMsg.asERR(String.format("Internal error: %s", err.getMessage()));
                 }
 
                 if (resp != null) {
-                    ostream.writeObject(resp);
+                    ostream.writeUnshared(resp);
+                    ostream.reset();
                 } else {
                     ostream.writeObject(ServerMsg.asERR(
                             String.format("Endpoint '%s %s' is not available.", req.getMethod(), req.getResource())));
@@ -63,7 +67,8 @@ public class InstructorSessionHandler extends SessionHandler {
         return ServerMsg.asOK("logout");
     }
 
-    private ServerMsg viewSections(ClientMsg req) {
-        return null;
+    private synchronized ServerMsg viewSections(ClientMsg req) {
+        var teaching = instructor.getTeaching().toArray(new Section[0]);
+        return ServerMsg.asOK(teaching);
     }
 }
